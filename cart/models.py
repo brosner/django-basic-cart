f17e308c398e6a0712f6f48c77c2d86997ba3649
brosner <brosner@gmail.com>
2008-08-14 09:37:18 -0600

    Added a generic relation on CartItem.

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,14 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
 class Cart(models.Model):
     user = models.ForeignKey(User)
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart)
+    object_pk = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType)
+    product = generic.GenericForeignKey("object_pk", "content_type")
